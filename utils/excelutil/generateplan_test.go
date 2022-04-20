c32a0e0cdb791f1abe6698ac9f5cdef168c502b4
yoloz <z88897050@gmail.com>
2022-04-20 11:15:09 +0800

    chage character case

---
+++ utils/excelutil/generateplan_test.go
@@ -0,0 +1,20 @@
+package excelutil
+
+import (
+	"log"
+	"path"
+	"testing"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+func TestGeneratePlan(t *testing.T) {
+	homedir.DisableCache = false
+	var month = [3]int{1, 2, 3}
+	dir, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var path = path.Join(dir, "plan.xlsx")
+	GeneratePlan(2022, month[:], path)
+}
---
+++ utils/httputil/readPost.go
@@ -0,0 +1,24 @@
+package httputil
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+//ReadForm checkForm values, false:no values,true:has values
+//after that,you can use  r.FormValue("xxx") get post value
+func ReadForm(r *http.Request) (bool, error) {
+	if err := r.ParseForm(); err != nil {
+		return false, err
+	}
+	return len(r.Form) != 0, nil
+}
+
+func ReadBody(r *http.Request) ([]byte, error) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
