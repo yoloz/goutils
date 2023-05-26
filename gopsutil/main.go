48304fa0be418845a9f8b38f7970a894e8e450c9
yoloz <z88897050@gmail.com>
2023-05-26 17:33:37 +0800

    Fri 26 May 2023 05:33:37 PM CST

--- gopsutil/main.go
+++ gopsutil/main.go
@@ -1,87 +1,44 @@
 package main
 
-import (
-	"container/list"
-	"encoding/json"
-	"fmt"
+import "fmt"
 
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/disk"
-	"github.com/shirou/gopsutil/v3/host"
-	"github.com/shirou/gopsutil/v3/mem"
-	"github.com/shirou/gopsutil/v3/net"
-	"github.com/shirou/gopsutil/v3/process"
+const (
+	KIBI uint64 = 1 << 10
+	MEBI uint64 = 1 << 20
+	GIBI uint64 = 1 << 30
+	TEBI uint64 = 1 << 40
+	PEBI uint64 = 1 << 50
+	EXBI uint64 = 1 << 60
 )
 
+// formatBytes into a rounded string representation using IEC standard
+func FormatBytes(bytes uint64) string {
+	if bytes == 1 { // bytes
+		return fmt.Sprintf("%d byte", bytes)
+	} else if bytes < KIBI { // bytes
+		return fmt.Sprintf("%d bytes", bytes)
+	} else if bytes < MEBI { // KiB
+		return formatUnits(bytes, KIBI, "KiB")
+	} else if bytes < GIBI { // MiB
+		return formatUnits(bytes, MEBI, "MiB")
+	} else if bytes < TEBI { // GiB
+		return formatUnits(bytes, GIBI, "GiB")
+	} else if bytes < PEBI { // TiB
+		return formatUnits(bytes, TEBI, "TiB")
+	} else if bytes < EXBI { // PiB
+		return formatUnits(bytes, PEBI, "PiB")
+	} else { // EiB
+		return formatUnits(bytes, EXBI, "EiB")
+	}
+}
+
+// formatUnits as exact integer or fractional decimal based on the prefix,appending the appropriate units
+func formatUnits(value uint64, prefix uint64, unit string) string {
+	if value%prefix == 0 {
+		return fmt.Sprintf("%d %s", value/prefix, unit)
+	}
+	return fmt.Sprintf("%.1f %s", float64(value)/float64(prefix), unit)
+}
+
 func main() {
-	// hostInfo()
-	// cpuInfo()
-	// memInfo()
-	// diskInfo()
-	// netInfo()
-	procInfo()
-
-}
-
-func hostInfo() {
-	fmt.Println("主机信息：")
-	host, _ := host.Info()
-	fmt.Printf(" %v\n", host)
-}
-
-func cpuInfo() {
-	fmt.Println("CPU信息：")
-	cpu, _ := cpu.Info()
-	fmt.Printf(" %v\n", cpu)
-}
-
-func memInfo() {
-	fmt.Println("内存信息：")
-	mem, _ := mem.VirtualMemory()
-	fmt.Printf(" %v\n", mem)
-}
-
-func diskInfo() {
-	fmt.Println("磁盘信息：")
-	//io counters 暂时不需要
-	// dev, _ := disk.IOCounters()
-	// fmt.Printf(" %v\n", dev)
-	fmt.Println("分区信息：")
-	part, _ := disk.Partitions(false)
-	fmt.Printf(" %v\n", part)
-}
-
-func netInfo() {
-	fmt.Println("网络信息：")
-	network, _ := net.Interfaces()
-	fmt.Printf(" %v\n", network)
-}
-
-func procInfo() {
-	fmt.Println("进程信息：")
-	var l list.List
-	ps, _ := process.Processes()
-	for _, p := range ps {
-		m := make(map[string]interface{}, 13)
-		m["pid"] = p.Pid
-		m["name"], _ = p.Name()
-		m["cmdline"], _ = p.Cmdline()
-		m["createtime"], _ = p.CreateTime()
-		m["cwd"], _ = p.Cwd()
-		m["env"], _ = p.Environ()
-		m["exe"], _ = p.Exe()
-		m["gids"], _ = p.Gids()
-		m["uids"], _ = p.Uids()
-		m["numfds"], _ = p.NumFDs()
-		m["numthreads"], _ = p.NumThreads()
-		m["ppid"], _ = p.Ppid()
-		m["username"], _ = p.Username()
-		l.PushBack(m)
-	}
-	fmt.Printf("[")
-	for i := l.Front(); i != nil; i = i.Next() {
-		pss, _ := json.Marshal(i.Value)
-		fmt.Printf("%s,", string(pss))
-	}
-	fmt.Printf("]")
 }
