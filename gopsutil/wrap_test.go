d19201aaddd520c8695d0295a76c5a3994c709e7
yoloz <z88897050@gmail.com>
2023-05-26 17:33:49 +0800

    Fri 26 May 2023 05:33:49 PM CST

---
+++ gopsutil/wrap_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHostInfo(t *testing.T) {
+	m, _ := HostInfo()
+	fmt.Printf("%v\n", m)
+}
+
+func TestCpuInfo(t *testing.T) {
+	m, _ := CpuInfo()
+	fmt.Printf("%v\n", m)
+
+}
+
+func TestCpuStat(t *testing.T) {
+	m, _ := CpuStat()
+	fmt.Printf("%v\n", m)
+}
+
+func TestMemInfo(t *testing.T) {
+	m, _ := MemInfo()
+	fmt.Printf("%v\n", m)
+}
+
+func TestDiskInfo(t *testing.T) {
+	m, _ := PartitionInfo()
+	fmt.Printf("%v\n", m)
+}
+
+func TestNCList(t *testing.T) {
+	m, _ := NCList()
+	fmt.Printf("%v\n", m)
+}
+
+func TestProcInfo(t *testing.T) {
+	m, _ := ProcessList()
+	fmt.Printf("%v\n", m)
+}
+
+func TestProcessInfo(t *testing.T) {
+	m, _ := ProcessInfo(1247)
+	fmt.Printf("%v\n", m)
+}
---
+++ gopsutil/wraphost.go
@@ -0,0 +1,50 @@
+package main
+
+//#include <stdlib.h>
+
+import (
+	"C"
+	"encoding/json"
+	"fmt"
+
+	"github.com/shirou/gopsutil/v3/host"
+)
+
+// HostInfo host info
+func HostInfo() (*map[string]interface{}, error) {
+	host, err := host.Info()
+	if err != nil {
+		return nil, err
+	}
+	m := make(map[string]interface{}, 13)
+	m["hostname"] = host.Hostname
+	m["hostid"] = host.HostID
+	m["uptime"] = host.Uptime
+	m["boottime"] = host.BootTime
+	m["procs"] = host.Procs
+	m["os"] = host.OS
+	m["platform"] = host.Platform
+	m["platformFamily"] = host.PlatformFamily
+	m["platformVersion"] = host.PlatformVersion
+	m["kernelArch"] = host.KernelArch
+	m["kernelVersion"] = host.KernelVersion
+	m["virtualizationSystem"] = host.VirtualizationSystem
+	m["virtualizationRole"] = host.VirtualizationRole
+	return &m, nil
+}
+
+//export CHostInfo
+func CHostInfo() *C.char {
+	m, err := HostInfo()
+	if err != nil {
+		fmt.Println("get host info fail..." + err.Error())
+		return C.CString("{}")
+	}
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("marshal host info fail..." + err.Error())
+		return C.CString("{}")
+	}
+	gostr := string(bytes)
+	return C.CString(gostr)
+}
