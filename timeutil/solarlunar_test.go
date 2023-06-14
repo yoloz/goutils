30080802ca61a101ad44d688b119e5ec71803da8
yoloz <z88897050@gmail.com>
2023-06-14 13:31:03 +0800

    v0.1.0

---
+++ timeutil/solarlunar_test.go
@@ -0,0 +1,14 @@
+package timeutil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolarToLuanr(t *testing.T) {
+	fmt.Println(SolarToLuanr(2021, 10, 15))
+}
+
+func TestLunarToSolar(t *testing.T) {
+	fmt.Println(LunarToSolar(2021, 9, 10, false))
+}
